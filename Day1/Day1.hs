87fa6d6bb91bd032c5375662660cba359c25c6ac
yusuke morizumi <y.morizumi@m.bokublock.jp>
2022-12-01 17:27:29 +0900

    day 1 part 2

--- Day1/Day1.hs
+++ Day1/Day1.hs
@@ -1,14 +1,28 @@
 -- stack script --resolver nightly-2022-11-08 --package bytestring --package extra
+
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.List             as L
 import qualified Data.List.Extra       as LE
 
 main = do
-    ans <- partOneSolve
-    print ans
-
+    -- ans1 <- partOneSolve
+    -- print ans1
+    ans2 <- partTwoSolve
+    print ans2
 
 partOneSolve :: IO Int
 partOneSolve = maximum . map sum' . LE.split (== Nothing) . map (fmap fst . BS.readInt) . BS.lines <$> BS.getContents
+    where
+        sum' = foldl add 0
+        sum' :: [Maybe Int] -> Int
+
+        add :: Int -> Maybe Int -> Int
+        add acc (Just n) = acc + n
+        add acc Nothing  = acc
+
+
+partTwoSolve :: IO Int
+partTwoSolve =  sum . take 3 . L.sortBy (flip compare) . map sum' . LE.split (== Nothing) . map (fmap fst . BS.readInt) . BS.lines <$> BS.getContents
     where
         sum' = foldl add 0
         sum' :: [Maybe Int] -> Int
