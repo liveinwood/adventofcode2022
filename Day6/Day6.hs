4a114faee251d8c1c1c2afa20fb9387c516ab08d
yusuke morizumi <y.morizumi@m.bokublock.jp>
2022-12-07 19:26:51 +0900

    Day 6 Part 1 done

---
+++ Day6/Day6.hs
@@ -0,0 +1,20 @@
+-- stack script --resolver nightly-2022-11-08 --package bytestring --package vector --package mtl --package transformers
+
+{-# LANGUAGE OverloadedStrings #-}
+
+-- import qualified Data.ByteString.Char8 as BS
+-- import qualified Data.Map as M
+import qualified Data.List as L
+
+solvePartOne :: String -> Int
+solvePartOne = go ""
+    where
+        go _ "" = error "error"
+        go acc (c:cs) | length acc <= 3 = go (c:acc) cs
+                      | unique (c: take 3 acc) = length (c:acc)
+                      | otherwise = go (c:acc) cs
+            where
+                unique cs = (length . L.nub) cs == length cs
+main = do
+    s <- getLine
+    print $ solvePartOne s
