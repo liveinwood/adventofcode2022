1a11d984e08f2eae6410795cebe4c14630a6a31a
yusuke morizumi <y.morizumi@m.bokublock.jp>
2022-12-13 04:30:13 +0900

    Day 10 Part 2 Done

--- Day10/Day10.hs
+++ Day10/Day10.hs
@@ -2,6 +2,7 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 
+import           Control.Monad
 import qualified Data.ByteString.Char8 as BS
 
 data Program = Noop | Addx Int
@@ -26,14 +27,47 @@ mkProgram = do
     where
         helper :: BS.ByteString -> Program
         helper s = case BS.words s of
-                        ["noop"] -> Noop
+                        ["noop"]    -> Noop
                         ["addx", n] -> Addx $ read (BS.unpack n)
-                        _ -> error "error"
+                        _           -> error "error"
 
--- solvePartOne :: [Program] -> [Int]
 solvePartOne :: [Program] -> Int
 solvePartOne programs = sum $ map (\(s, v) -> s * v) $ signale 0 1 programs [20, 60, 100, 140, 180, 220] []
 
+
+signalePart2 :: Int -> [Program] -> [Int]-> [Int]
+signalePart2 val [] acc = acc
+signalePart2 val (p:ps) acc =
+    case p of
+        Noop   -> signalePart2 val ps (val:acc)
+        Addx n -> signalePart2 (val + n) ps (val:val:acc)
+
+stripe :: Int -> String
+stripe n = replicate (n - 1) '.' ++ "###" ++ replicate (40 - n - 1) '.'
+
+crt :: Int ->  String
+crt n = replicate (n - 1) '.' ++ "#" ++ replicate (40 - n + 1) '.'
+
+divideSignal []      = []
+divideSignal signale = take 40 signale : divideSignal (drop 40 signale)
+
+draw :: [Int] -> [String]
+draw signale = map (foldl helper2 (replicate 40 '.')) $ map (\sig -> zipWith helper (map crt [1..]) (map stripe sig)) (divideSignal signale)
+    where
+        helper :: String -> String -> String
+        helper ss cc = zipWith helper' ss cc
+        helper' '#' '#' = '#'
+        helper' _ _     = '.'
+
+        helper2 :: String -> String -> String
+        helper2 ss cc = zipWith helper'' ss cc
+        helper'' '#' _ = '#'
+        helper'' _ '#' = '#'
+        helper'' _ _   = '.'
+
 main = do
     p <- mkProgram
-    print $ solvePartOne p
+    let signale = reverse $ signalePart2 1 p []
+    let img = draw signale
+    forM_ img $ \l -> do
+        putStrLn l
